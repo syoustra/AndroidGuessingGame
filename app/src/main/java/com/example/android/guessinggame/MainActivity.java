b19cfc8d4820815d9c52d4e7bfe46f81cead49dc
Stephanie Youstra <steph.you07@gmail.com>
2018-03-13 16:30:01 -0400

    Created GameActivity and associated xml

--- app/src/main/java/com/example/android/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/android/guessinggame/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.guessinggame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startGame();
+    }
+
+    private void startGame() {
+        Intent playGame = new Intent(this, GameActivity.class);
+        startActivity(playGame);
     }
 }
