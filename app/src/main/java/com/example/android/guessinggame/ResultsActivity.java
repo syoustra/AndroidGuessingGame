7c36929cf67be4b7817f66dc6d14c3187f9676e0
Stephanie Youstra <steph.you07@gmail.com>
2018-03-16 16:33:09 -0400

    Customized ResultsActivity xml elements

--- app/src/main/java/com/example/android/guessinggame/ResultsActivity.java
+++ app/src/main/java/com/example/android/guessinggame/ResultsActivity.java
@@ -2,12 +2,39 @@ package com.example.android.guessinggame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
+
+    private Button playAgainButton;
+    private TextView correctNumberTextview;
+    private TextView resultsTextview;
+    private ImageView resultImageview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        playAgainButton = findViewById(R.id.restart_button);
+        correctNumberTextview = findViewById(R.id.number_textview);
+        resultsTextview = findViewById(R.id.results_textview);
+        resultImageview = findViewById(R.id.winning_imageview);
+
+        setListener();
     }
+
+    private void setListener() {
+        playAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
 }
